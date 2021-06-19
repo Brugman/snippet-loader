94565afe447252f69f0974a5b93f1239075193b1
Tim Brugman <tim@timbrugman.com>
2021-06-19 22:29:37 +0200

    Simplify code for active

--- snippets/snippet-template.php
+++ snippets/snippet-template.php
@@ -3,15 +3,13 @@
 if ( !defined( 'ABSPATH' ) )
     exit;
 
-$active = false;
-
 $snippets_meta[ __FILE__ ] = [
     'name'   => 'Snippet Template',
     'desc'   => 'Copy the code from this template into your snippets to display them properly in the backend.',
-    'active' => $active,
+    'active' => false,
 ];
 
-if ( !$active )
+if ( !$snippets_meta[ __FILE__ ]['active'] )
     return;
 
 /**
